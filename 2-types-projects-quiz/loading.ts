6f03c413dc819ce06cd1fb0e6310cc84bcdd8851
song <55rsearchco@gmail.com>
2022-05-15 21:01:34 +0900

    Type 챌린지 End.

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -20,6 +20,19 @@
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
+  function printLoginState(state: ResourceLoadState): void {
+    const result = state.state;
+    if (result === 'loading') {
+      console.log('👀 loading...');
+    } else if (result === 'success') {
+      console.log(`${state.response.body}`);
+    } else if (result === 'fail') {
+      console.log(`${state.reason}`);
+    } else {
+      throw new Error(`unknown state: ${state}`);
+    }
+  }
+
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
