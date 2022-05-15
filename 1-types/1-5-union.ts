50e3aa83e34e08a14b2baef8cc40eb9be126def7
song <55rsearchco@gmail.com>
2022-05-15 15:36:22 +0900

    진정한 타입스크립트의 시작! Union 타입

---
+++ 1-types/1-5-union.ts
@@ -0,0 +1,32 @@
+{
+  /**
+   * Union Types: OR
+   * 발생할 수 있는 case 들 중 하나만 사용할 때.
+   */
+  type Direction = 'left' | 'right' | 'up' | 'down';
+  function move(direction: Direction) {
+    console.log(direction);
+  }
+  move('down');
+
+  type TileSize = 8 | 16 | 32;
+  const tile: TileSize = 16;
+
+  // function: login -> success, fail
+  type SuccessState = {
+    response: {
+      body: string;
+    };
+  };
+  type FailState = {
+    reason: string;
+  };
+  type LoginState = SuccessState | FailState;
+  function login(id: string, password: string): LoginState {
+    return {
+      response: {
+        body: 'logged in!',
+      },
+    };
+  }
+}
