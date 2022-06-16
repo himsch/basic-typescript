b02906a42c86daf849c015cabd7a6ef89ec082fd
song <55rsearchco@gmail.com>
2022-06-16 23:32:01 +0900

    Favor COMPOSITION over inheritance

---
+++ 3-OOP/3-7-composition.ts
@@ -0,0 +1,131 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+    hasSugar?: boolean;
+  };
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  class CoffeeMachine implements CoffeeMaker {
+    private static BEANS_GRAM_PER_SHOT: number = 7; // class level
+    private coffeeBeans: number = 0;  // instance(Object) level
+
+    // 생성자가 private 이면, new 키워드를 통해 인스턴스를 생성 할 수 없다.
+    public constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMaker {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error(`value for beans should be greater than 0`);
+      }
+      this.coffeeBeans += beans;
+    }
+
+    clean() {
+      console.log('cleaning the machine...🤪');
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAM_PER_SHOT) {
+        throw new Error('Not enough coffee beans!');
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAM_PER_SHOT;
+    }
+
+    private preheat(): void {
+      console.log('heating up...');
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling ${shots} shots....`);
+      return {
+        shots,
+        hasMilk: false,
+      }
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+  }
+
+  // 싸구려 우유 거품기
+  class CheapMilkSteamer {
+    private steamMilk(): void {
+      console.log('Steaming some milk....');
+    }
+    makeMilk(cup: CoffeeCup): CoffeeCup {
+      this.steamMilk();
+      return {
+        ...cup,
+        hasMilk: true,
+      }
+    }
+  }
+
+  // 설탈 제조기
+  class AutomaticSugarMixer {
+    private getSugar() {
+      console.log('Getting some sugar form candy🍬');
+    }
+    addSugar(cup: CoffeeCup): CoffeeCup {
+      const sugar = this.getSugar();
+      return {
+        ...cup,
+        hasSugar: true,
+      }
+    }
+  }
+
+  class CaffeLatteMachine extends CoffeeMachine {
+    constructor(
+      beans: number,
+      public readonly serialNumber: string,
+      private milkFrothier: CheapMilkSteamer
+    ) {
+      super(beans);
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      return this.milkFrothier.makeMilk(coffee);
+    }
+  }
+
+  class SweetCoffeeMaker extends CoffeeMachine {
+    constructor(private beans: number, private sugar: AutomaticSugarMixer) {
+      super(beans);
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      return this.sugar.addSugar(coffee);
+    }
+
+  }
+
+  class SweetCaffeLatteMachine extends CoffeeMachine {
+    constructor(
+      private beans: number,
+      private milk: CheapMilkSteamer,
+      private sugar: AutomaticSugarMixer
+    ) {
+      super(beans);
+    }
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      return this.milk.makeMilk(this.sugar.addSugar(coffee));
+    }
+  }
+}
