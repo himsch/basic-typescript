7b9f9d563496c7477beaac0c068e42624e512154
song <55rsearchco@gmail.com>
2022-06-19 22:35:54 +0900

    구현했던 Stack 자료구조를 제네릭을 사용해서 여러 타입을 받도록 개선.

---
+++ 5-generic-project/stack.ts
@@ -0,0 +1,56 @@
+{
+  interface Stack<T> {
+    readonly size: number;
+    push(value: T): void;
+    pop(): T;
+  }
+
+  type StackNode<T> = {
+    readonly value: T;
+    readonly next?: StackNode<T>;
+  }
+
+  class StackImpl<T> implements Stack<T> {
+    private _size: number = 0;
+    private head?: StackNode<T>;
+
+    constructor(private capacity: number) {
+    }
+    get size(): number {
+      return this._size;
+    }
+    push(value: T) {
+      if (this.size === this.capacity) {
+        throw new Error('Stack is full!');
+      }
+      this.head = {
+        value,
+        next: this.head,
+      };
+      this._size++;
+    }
+    pop(): T {
+      if (this.head == null) {
+        throw new Error('Stack is empty!!');
+      }
+      const node = this.head;
+      this.head = node.next;
+      this._size--;
+      return node.value;
+    }
+  }
+  const stack = new StackImpl<string>(10);
+  stack.push('Song 1');
+  stack.push('Hong 2');
+  stack.push('Gyu 3');
+  while(stack.size !== 0) {
+    console.log(stack.pop());
+  }
+  const stack2 = new StackImpl<number>(10);
+  stack2.push(123);
+  stack2.push(456);
+  stack2.push(789);
+  while(stack2.size !== 0) {
+    console.log(stack2.pop());
+  }
+}
