c17ecca935862d4a7096c5c403857275baf6d0a2
song <55rsearchco@gmail.com>
2022-06-19 22:04:02 +0900

    제네릭에 조건(constrain)을 사용해보자

---
+++ 5-generic/5-3-constrains.ts
@@ -0,0 +1,43 @@
+{
+  interface Employee {
+    pay(): void;
+  }
+
+  class FullTimeEmployee implements Employee {
+    pay() {
+      console.log(`full time!!`);
+    }
+    workFullTime() {
+
+    }
+  }
+
+  class PartTimeEmployee implements Employee {
+    pay() {
+      console.log(`part time!!`);
+    }
+    workPartTime() {
+
+    }
+  }
+
+  // 세부적인 타입을 인자로 받아서 정말 추상적인 타입으로 다시 리턴하는 함수는 💩
+  function payBad(employee: Employee): Employee {
+    employee.pay();
+    return employee;
+  }
+
+  function pay<T extends Employee>(employee: T): T {
+    employee.pay();
+    return employee;
+  }
+
+  const song = new FullTimeEmployee();
+  const bob = new PartTimeEmployee();
+  song.workFullTime();
+  bob.workPartTime();
+  const songAfterPay = payBad(song);
+  const bobAfterPay = payBad(bob);
+  const songAfterPay2 = pay(song);
+  songAfterPay2.workFullTime();
+}
