3794ebe58aaf634948063a89562b9e83760f09ee
song <55rsearchco@gmail.com>
2023-05-25 09:46:05 +0900

    Index Type

---
+++ 9-advenced/9-2-index.ts
@@ -0,0 +1,30 @@
+{
+  const obj = {
+    name: "song",
+  };
+  obj.name;
+  obj["name"];
+
+  type Animal = {
+    name: string;
+    age: number;
+    gender: "male" | "female";
+  };
+
+  type Name = Animal["name"]; // string
+  const text: Name = "";
+
+  type Gender = Animal["gender"]; // 'male' | 'female'
+
+  type Keys = keyof Animal; // 'name' | 'age' | 'gender'
+  const key: Keys = "gender";
+
+  type Person = {
+    name: string;
+    gender: Animal["gender"];
+  };
+  const person: Person = {
+    name: "song",
+    gender: "male",
+  };
+}
